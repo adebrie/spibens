b6ba68b462d0f589eb508046eb376d31339993f3
Unknown <pbest@ece.fr>
2017-04-17 00:28:20 +0200

    members activity

--- app/src/main/java/debrie/android/fr/spibens/Members.java
+++ app/src/main/java/debrie/android/fr/spibens/Members.java
@@ -3,23 +3,81 @@ package debrie.android.fr.spibens;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Objects;
 
 public class Members extends AppCompatActivity {
+
+    private DatabaseReference membersRef;
+    private ArrayList<String> members;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
 
-        ImageView patrick = (ImageView) findViewById(R.id.patrickProfile);
-        patrick.setOnClickListener(new View.OnClickListener() {
+        membersRef = FirebaseDatabase.getInstance().getReference("membersList");
+
+
+
+
+        membersRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Members.this, Profile.class);
-                startActivity(i);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                User user = new User((Map<String, Object>) dataSnapshot.getValue());
+                members.add(user.getName());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
+        ListIterator  iterator = members.listIterator();
+        while(iterator.hasNext()){
+            ImageView patrick = (ImageView) findViewById(R.id.patrickProfile);
+            patrick.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Members.this, Profile.class);
+                    startActivity(i);
+                }
+            });
+        }
+        GridView grid = (GridView)findViewById(R.id.grid);
+        grid.setAdapter(new ArrayAdapter<String>(this, R.layout.activitylist, (String[]) members.toArray()));
     }
+
 }
