7762f4d3f506c696c3d66eed54735f37edf43a5e
Unknown <pbest@ece.fr>
2017-06-22 23:23:28 +0200

    subscribe HH

--- app/src/main/java/debrie/android/fr/spibens/HappyHourActivity.java
+++ app/src/main/java/debrie/android/fr/spibens/HappyHourActivity.java
@@ -20,6 +20,12 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
@@ -30,12 +36,43 @@ import java.io.IOException;
 
 public class HappyHourActivity extends AppCompatActivity {
 
-    boolean starred = false;
+    boolean starred;
+    DatabaseReference SubscribeRef;
+    boolean done=false;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.happyh, menu);
+        if(!done) {
+            SubscribeRef = FirebaseDatabase.getInstance().getReference("membersList").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("happyhour");
+            SubscribeRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.getValue().equals(true)) {
+                        starred = true;
+                        invalidateOptionsMenu();
+                        done = true;
+                    } else {
+                        starred = false;
+                        invalidateOptionsMenu();
+                        done = true;
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+        if(starred){
+            menu.getItem(0).setIcon(R.drawable.ic_action_star);
+            starred=true;
+        }else{
+            menu.getItem(0).setIcon(R.drawable.ic_action_star_border);
+            starred=false;
+        }
         return true;
     }
 
@@ -48,6 +85,9 @@ public class HappyHourActivity extends AppCompatActivity {
 
         final StorageReference mStorageRef;
         mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://spibens-331c8.appspot.com/").child("HappyHour").child("flyer.png");
+
+
+
 
         ImageView im = (ImageView) findViewById(R.id.flyer_im);
         Glide.with(this)
@@ -65,14 +105,16 @@ public class HappyHourActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_favorite:
+            case R.id.action_favorite420:
                 FirebaseMessaging fm = FirebaseMessaging.getInstance();
                 if (!starred) {
+                    SubscribeRef.setValue(true);
                     item.setIcon(R.drawable.ic_action_star);
                     fm.subscribeToTopic("HappyHour");
                     Toast.makeText(getApplicationContext(), "Subscribed to HappyHour", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    SubscribeRef.setValue(false);
                     item.setIcon(R.drawable.ic_action_star_border);
                     fm.unsubscribeFromTopic("HappyHour");
                     Toast.makeText(getApplicationContext(), "Unsubscribed from HappyHour", Toast.LENGTH_SHORT).show();
